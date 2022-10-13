c054f5bf231c2011d3ce02488d8e9c7aadf38a06
DiegoCruz02 <a.diegocruz02@gmail.com>
2022-10-13 18:51:12 +0000

    Directors Show works! Used ruby gems to generate SQL data and made our app display directors through the database.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get("/directors/eldest",{ :controller => "directors", :action => "wisest"})
+
+  get("/directors/youngest",{ :controller => "directors", :action => "youthful"})
+
+  get("/directors/:an_id",{ :controller => "directors", :action => "director_details"})
+  
   get("/", { :controller => "application", :action => "homepage" })
 
   get("/directors",{ :controller =>"directors", :action => "index"})
+
 end
